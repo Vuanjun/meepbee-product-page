96d50bef9b43103a1afeb64d20285bf8f72da095
Victor <victor@meepshop.com>
2015-01-09 20:45:24 +0800

    tried resize and load

--- public/js/transaction.js
+++ public/js/transaction.js
@@ -38,17 +38,40 @@ paymentSummaryCofrimBtn.onclick = function() {
 
   var paymentSummary = document.getElementsByClassName('js-payment-summary')[0];
   var recipientInfo = document.getElementsByClassName('js-recipientInfo')[0];
+  var comment = document.getElementsByClassName('js-comment')[0];
+
+  var product = document.getElementsByClassName('js-product')[0];
+  var carouselBox = document.getElementsByClassName('js-carouselBox')[0];
+
 
   if(!paymentDetailIsOpen) {
     paymentDetailIsOpen = true;
     paymentSummaryCofrimBtn.innerHTML = "關閉資訊";
+
+    // if(window.innerWidth>=1024 && window.innerWidth<=1300) comment.style.marginTop="-62px";
     paymentSummary.style.display="block";
     recipientInfo.style.display="block";
   } else {
     paymentDetailIsOpen = false;
     paymentSummaryCofrimBtn.innerHTML = "計算價錢";
+
+    // if(window.innerWidth>=1024 && window.innerWidth<=1300) comment.style.marginTop="12px";
     paymentSummary.style.display="none";
     recipientInfo.style.display="none";
   }
 
+}
+
+window.addEventListener('load', adjustCommentPosition);
+
+function adjustCommentPosition() {
+  var product = document.getElementsByClassName('js-product')[0];
+  var carouselBox = document.getElementsByClassName('js-carouselBox')[0];
+  var airBetween = 65-(product.offsetHeight - carouselBox.offsetHeight);
+  var comment = document.getElementsByClassName('js-comment')[0];
+  console.log(airBetween);
+  if(airBetween<=221 && window.innerWidth>=1024 && window.innerWidth<=1300) {
+    console.log('in');
+    comment.style.marginTop= airBetween + 'px';
+  }
 }
