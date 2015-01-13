2c7485948bb2b0d35e8fe6a326bcf414762eb4f1
Nukr <nukrs.w@gmail.com>
2015-01-13 13:55:15 +0800

    add more table data into product

--- app.js
+++ app.js
@@ -10,23 +10,49 @@ var Parse = require('parse-restapi');
 var keys = require('./key');
 var meepbee = new Parse(keys.appId, keys.restKey);
 var thunkify = require('thunkify');
-var getObject = thunkify(meepbee.classes('Products').get);
+var getProduct = thunkify(meepbee.classes('Products').getProduct);
+var getComment = thunkify(meepbee.classes('Comments').getAll);
+var getLike = thunkify(meepbee.classes('Likes').getLike);
+var getUser = thunkify(meepbee.users().get);
+
 
 app.use(router(app));
 app.use(mount('/public', serve(__dirname + '/public')));
 
-
 app.get('/', function* (next) {
-    var html = yield render('src/home/index.jade');
-    this.body = html;
+  var html = yield render('src/home/index.jade');
+  this.body = html;
 });
 
 app.get('/:productId', function* (next) {
-    var productId = this.params.productId;
-    var obj = yield getObject(productId);
-    var html = yield render('src/home/index.jade', {product: obj[0].body});
-    this.body = html;
-    //this.body = obj[0].body;
+  var productId = this.params.productId;
+
+  var product = yield getProduct(productId);
+  product = JSON.parse(product[0].body);
+  product.comments = [];
+  product.like = []
+
+
+  var comments = yield getComment();
+  comments = JSON.parse(comments[0].body).results;
+  comments.forEach(function (comment) {
+    if (comment.product.objectId === productId) {
+      product.comments.push(comment)
+    }
+  });
+
+  var likes = yield getLike();
+  likes = JSON.parse(likes[0].body).results;
+  likes.forEach(function (like) {
+    if (like.likedProduct.objectId === productId) {
+      product.like.push(like);
+    }
+  });
+
+  var html = yield render('src/home/index.jade', {
+    product: JSON.stringify(product)
+  });
+  this.body = html;
 });
 
 console.log('Hey sailor, Your server is on at port: 9000');
